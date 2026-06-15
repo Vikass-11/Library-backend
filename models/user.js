13ed856cdaa5b11991bd8fcfd4209097e1a2050f
Vikas <vikas111006@gmail.com>
2026-06-15 19:23:03 +0530

    feat: create user model with password hashing

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'member'],
+        default: 'member',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+//Hash password
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Method to compare passwords
+userSchema.methods.comparePassword = async function (plainPassword) {
+    return await bcrypt.compare(plainPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
