9c040a79dee3ef21181250a6979f17e037518df9
Your Name <your-email@example.com>
2026-06-15 12:29:29 +0530

    fix: correct authors route from /author to /authors

--- routes/libraryRoutes.js
+++ routes/libraryRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const libraryController = require('../controllers/libraryController.js');
 router.get('/books', libraryController.getAllBooks);
 router.get('/books/:id', libraryController.getBookById);
-router.get('/author', libraryController.getAllAuthors);
+router.get('/authors', libraryController.getAllAuthors);
 module.exports =router;
